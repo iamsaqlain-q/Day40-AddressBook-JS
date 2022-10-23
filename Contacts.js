1a8951494abbcbf582bdab892362cc3c59340ae4
Saqlain <iamkhatik786@gmail.com>
2022-10-23 19:21:02 +0530

    Validate Input

--- Contacts.js
+++ Contacts.js
@@ -41,37 +41,70 @@ class Contact{
     }
 
     set firstName(firstName){
-        this._firstName = firstName;
+        const nameRegex = RegExp('^[A-Z][a-zA-Z]{2,}$');
+        if (nameRegex.test(firstName))
+            this._firstName = firstName;
+        else 
+            throw 'Name input is Invalid!';
     }
 
     set lastName(lastName){
-        this._lastName = lastName;
+        const nameRegex = RegExp('^[A-Z][a-zA-Z]{2,}$');
+        if (nameRegex.test(lastName))
+            this._lastName = lastName;
+        else 
+            throw 'Name input is Invalid!';    
     }
 
     set city(city){
+        const cityStateRegex = RegExp('^[a-zA-z]{3,}$');
+        if (cityStateRegex.test(city))
         this._city = city;
+        else
+            throw "City input is Invalid!";   
     }
 
     set state(state){
+        const cityStateRegex = RegExp('^[a-zA-z]{3,}$');
+        if (cityStateRegex.test(state))
         this._state = state;
+        else
+            throw "State input is Invalid!";    
     }
 
     set zip(zip){
+        const zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+        if (zipRegex.test(zip))
         this._zip = zip;
+        else
+            throw "Zip input is Invalid!";    
     }
 
     set phoneNumber(phoneNumber){
+        const phoneNumberRegex = RegExp('^[0-9]{2}\\s{1}[0-9]{10}$');
+        if (phoneNumberRegex.test(phoneNumber))
         this._phoneNumber = phoneNumber;
-    }
+        else
+            throw "Phone number input is Invalid!";    }
 
     set email(email){
-        this._email = email;
+        const emailRegex = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$";
+        if (emailRegex.test(email))
+            this._email = email;
+        else
+            throw "Email input is Invalid!";
     }
+
 
     toString(){
         return "First Name : " + this.firstName + "\nLast Name : " + this.lastName + "\nCity : " + this.city + "\nState : " + this.state + "\nZip : "+ this.zip+ "\nPhone Number : " + this.phoneNumber + "\nEmail ID: " + this.email;
     }
 }
 
-let contact = new Contact("Tushar", "Badgujar", "Mumbai", "Maharashtra", 250001, 987654321, "tushar@gmail.com");
-console.log(contact.toString());
+try{
+	let contact = new Contact("Tushar", "Badgujar", "Mumbai", "Maharashtra", 250001, 9876543210, "tushar@gmail.com");
+	console.log(contact.toString());
+}
+catch(e){
+	console.log(e);
+}
